317f06b803e5c2b72869a5722357fa4e84e9b34e
ayushi81008-cell <ayushi81008@gmail.com>
2026-04-06 11:08:37 +0530

    Added UC5 implementation

--- src/main/java/com/HelloApp/HelloApp.java
+++ src/main/java/com/HelloApp/HelloApp.java
@@ -2,12 +2,22 @@ package com.HelloApp;
 public class HelloApp {
 
     public static void main(String[] args) {
-        String names = "World";
+        String finalName = "World";
 
         if (args.length > 0) {
-            names = String.join(", ", args);
+            StringBuilder nameBuilder = new StringBuilder();
+            boolean first = true;
+
+            for (String name : args) {
+                if (!first) {
+                    nameBuilder.append(", ");
+                }
+                nameBuilder.append(name);
+                first = false;
+            }
+            finalName = nameBuilder.toString();
         }
 
-        System.out.println("Hello, " + names + "!");
+        System.out.println("Hello, " + finalName + "!");
     }
 }
